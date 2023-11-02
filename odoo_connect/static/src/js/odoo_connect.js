cc4eab27eb8f0882197c95b812648572e5e554a2
shayma26 <tradshayma@gmail.com>
2023-11-02 14:03:44 +0100

    remove footer

---
+++ odoo_connect/static/src/js/odoo_connect.js
@@ -0,0 +1,17 @@
+odoo.define('odoo_connect.hide_documentation_footer', function (require) {
+    'use strict';
+
+    $(document).ready(function () {
+        // Get the current page's URL
+        var currentPageUrl = window.location.pathname;
+
+        // Extract the unique identifier from the URL
+        var uniqueIdentifier = currentPageUrl.match(/\/documentation\/(\d+)/);
+
+        // Check if a unique identifier was found and hide the footer if it matches a specific value (e.g., 2)
+        if (uniqueIdentifier) {
+            // Hide the footer element (replace 'footer-selector' with the actual selector of your footer)
+            $('footer').hide();
+        }
+    });
+});
