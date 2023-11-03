bad8b8e71cd58032c1f84f64b2dafa0534367105
shayma26 <tradshayma@gmail.com>
2023-11-03 15:18:01 +0100

    new module: add qrcode to product

---
+++ product_custom/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'ProductCustom',
+    'depends':['base','product'],
+    'data':[
+        'views/product_template_views.xml',
+
+        'report/product_product_templates.xml'
+    ],
+    'installable': True,
+    'application': True
+}
---
+++ product_custom/wizard/product_label_layout.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from odoo import models
+class ProductLabelLayout(models.TransientModel):
+    _inherit = 'product.label.layout'
