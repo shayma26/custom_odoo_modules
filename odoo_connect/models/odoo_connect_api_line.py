ba6132992a5d984ed56f99e67484f3f124186f36
shayma26 <tradshayma@gmail.com>
2023-11-13 13:27:16 +0100

    convert preview fields types from string to json

--- odoo_connect/models/odoo_connect_api_line.py
+++ odoo_connect/models/odoo_connect_api_line.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import ast
+import json
 from odoo import models, fields, api
 from string import capwords
 from ..tools.tools import format_data, replace_last, insert_after
@@ -48,7 +49,6 @@ class OdooConnectApiLine(models.Model):
                 vals['description'] = capwords(vals.get('description'))
             if not vals.get('description') and vals.get('name'):
                 vals['description'] = capwords(vals.get('name').replace("_", " "))
-            print("vals_list", vals)
         return super().create(vals_list)
 
     def write(self, vals):
@@ -98,7 +98,14 @@ class OdooConnectApiLine(models.Model):
                     record.response_preview = "%s File" % record.report_type.upper()
                 if record.report_response_type == 'url':
                     report = 'report/'
-                    record.response_preview = '''{\n"jsonrpc": "2.0",\n"id": id,\n"result": {\n\t"success": "true",\n\t"error": "",\n\t"data":"url""}\n}'''
+                    # record.response_preview = '''{\n"jsonrpc": "2.0",\n"id": id,\n"result": {\n\t"success": "true",\n\t"error": "",\n\t"data":"url""}\n}'''
+                    record.response_preview = str({
+                        "jsonrpc": "2.0",
+                        "id": 'id',
+                        "result": {
+                            "success": "true",
+                            "error": "",
+                            "data": "url"}})
             else:
                 method = record.method.upper() + ' '
             record.request_preview = method
@@ -108,32 +115,37 @@ class OdooConnectApiLine(models.Model):
     @api.onchange('method', 'fields_ids', 'model_id')
     def _onchange_body_response_preview(self):
         for record in self:
-            record.response_preview = '''{\n"jsonrpc": "2.0",\n"id": id,\n"result": {\n\t"success": "true",
-            \n\t"error": "",\n\t"data":\n\t}\n}'''
-            response_data = ""
+            # record.response_preview = '''{\n"jsonrpc": "2.0",\n"id": id,\n"result": {\n\t"success": "true",
+            # \n\t"error": "",\n\t"data":\n\t}\n}'''
+            response_preview = {
+                        "jsonrpc": "2.0",
+                        "id": 'id',
+                        "result": {
+                            "success": "true",
+                            "error": ""}}
+            body_preview = {}
+
             if record.method == 'get':
-                record.body_preview = '''{\n\t"page_size": {int:page_size},\n\t"page_number": {int:page_number}\n}'''
+                body_preview = {"page_size": "int_value","page_number": "int_value"}
+                response_preview['result']['data'] = [{}]
                 if record.fields_ids:
                     fields = record.fields_ids
                 else:
                     fields = record.model_id.field_id
-                response_data = '[\n'
                 for field in fields:
-                    response_data += '''\t\t"%s": {%s:%s_value},\n''' % (field.name, field.ttype, field.name)
-                response_data = replace_last(response_data, ',', '\n')
-                response_data += '''\t\t"total_records": {int:total_records}\n\t\t"total_pages": {int:total_records}\n\t]'''
+                    response_preview['result']['data'][0]["%s" % field.name] = "%s_value" % field.ttype
+                response_preview['result']['data'].append({"total_records":"int_value","total_pages":"int_value"})
             else:
                 if record.fields_ids and (record.method == 'post' or record.method == 'put'):
-                    record.body_preview = '{\n'
                     for field in record.fields_ids:
-                        record.body_preview += '''\t"%s": {%s:%s_value},\n''' % (field.name, field.ttype, field.name)
-                    record.body_preview = replace_last(record.body_preview, ',', '\n}')
+                        body_preview[field.name] = "%s_value" % field.ttype
                 if record.method == 'post':
-                    response_data = '''{"ids":[id]}'''
+                    response_preview['result']['data'] = {"ids":"[id]"}
                 if record.method == 'put' or record.method == 'delete':
-                    response_data = '''{"id":id}'''
+                    response_preview['result']['data'] = {"id":"id"}
 
-            record.response_preview = insert_after(record.response_preview, response_data, '"data":')
+            record.response_preview = json.dumps(response_preview)
+            record.body_preview = json.dumps(body_preview)
 
     def api_action(self, method, user, record_id=None, vals=None):
         model_obj = self.env[self.model_id.model]
@@ -164,7 +176,7 @@ class OdooConnectApiLine(models.Model):
                              range(0, len(result), vals.get('page_size'))]
                     total_pages = len(pages)
                     result = pages[vals.get('page_number') - 1]
-                    result.insert(len(result), {'total_records': total_records, "total_pages": total_pages})
+                    result.append({'total_records': total_records, "total_pages": total_pages})
                 return result
         if method == 'POST':
             return {'ids': model_obj.create(vals).ids}
--- odoo_connect/static/src/preview/preview.js
+++ odoo_connect/static/src/preview/preview.js
@@ -1,52 +1,58 @@
 /** @odoo-module */
 
 import { registry } from "@web/core/registry";
-import { useService } from "@web/core/utils/hooks";
+//import { useService } from "@web/core/utils/hooks";
 
 const { Component, onWillUpdateProps, onWillStart, useRef, onMounted } = owl;
 export class Preview extends Component {
 
+     parseAndStringify(input) {
+        try {
+            console.log("input",input)
+            // Attempt to parse the input as JSON
+            const parsed = JSON.parse(input);
+
+            // If parsing succeeds, stringify it again
+            const prettyJSON = JSON.stringify(parsed, null, 4);
+            console.log("pretty",prettyJSON)
+            return prettyJSON
+        } catch (error) {
+            // If parsing fails, return the input as it is
+            console.log("error",error)
+            return input;
+        }
+    }
+
     async setup(){
-        this.orm = useService("orm");
+//        this.orm = useService("orm");
         var api_line_model = this.env.model
         this.api_line_data = api_line_model.root.data
         this.targetContainer = useRef("targetContainer");
 
-//        onWillStart(() => {
-//            if (this.targetContainer.el) {
-//                this.targetContainer.el.value = this.props.value;
-//                Prism.highlightAll()
-//                }
-//            });
-
         onWillUpdateProps((nextProps) => {
 
             // you can access to other fields in nextProps.record
-            console.log("update props", nextProps.value)
-            this.targetContainer.el.textContent = nextProps.value;
+            this.targetContainer.el.textContent = this.parseAndStringify(nextProps.value);
             Prism.highlightAll()
         });
 
 
         onMounted(() => {
                 if (this.targetContainer.el) {
-                    console.log("mounted", this.props.value)
-                    this.targetContainer.el.textContent = this.props.value;
+                    this.targetContainer.el.textContent = this.parseAndStringify(this.props.value);
                     Prism.highlightAll()
                 }
         });
 
     }
 
-    async getData() {
-        const domain = this.api_line_data.domain != "" ? eval(this.api_line_data.domain.replace('(','[').replace(')',']')) : []
-        const fields = await this.orm.searchRead('ir.model.fields',[['id','in',this.api_line_data.fields_ids.records.map(item => item.data.id)]],['name'])
-        const fields_names = fields.map(item => item.name)
-        const results = await this.orm.searchRead(this.api_line_data.model_name, domain, fields_names);
-        return fields_names
-    }
-
-
+//    async getData() {
+//        const domain = this.api_line_data.domain != "" ? eval(this.api_line_data.domain.replace('(','[').replace(')',']')) : []
+//        const fields = await this.orm.searchRead('ir.model.fields',[['id','in',this.api_line_data.fields_ids.records.map(item => item.data.id)]],['name'])
+//        const fields_names = fields.map(item => item.name)
+//        const results = await this.orm.searchRead(this.api_line_data.model_name, domain, fields_names);
+//        return fields_names
+//    }
 }
 Preview.template = "odoo_connect.Preview";
 Preview.supportedTypes = ["text"];
